20b3a282e2e59e577d58f66fdac823d58db8cd05
Jesse Wenger <wengerj@gmail.com>
2021-03-10 12:38:21 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,12 +1,10 @@
 class Meal
-
-  #attr_accessor :water, :customer, :total, :tip
+  attr_accessor :water, :customer, :total, :tip
   
-  initialize(water, customer, total, tip)
+  def initialize(water, customer, total, tip)
     @water = water
     @customer = customer
     @total = total
     @tip = tip
   end
-
 end
