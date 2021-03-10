f4552030ac6cc288a5f38d4015bb1f760eef1938
Jesse Wenger <wengerj@gmail.com>
2021-03-10 12:35:42 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,10 @@
 class Meal
-
+  attr_accessor :water, :customer, :total, :tip
+  
+  initialize(water, customer, total, tip)
+    @water = water
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
 end
